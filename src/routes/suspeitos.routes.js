277cf3ca4c9efb2f4b61b98372bf88aad4d7e037
Fernanda Louro <fernandaalves.tds2.senai@gmail.com>
2024-10-10 15:46:51 -0300

    feat: create post method

--- src/routes/suspeitos.routes.js
+++ src/routes/suspeitos.routes.js
@@ -4,14 +4,46 @@ const suspeitosRoutes = Router();
 
 // Array com futuros suspeitos
 let suspeitos = [
-    {
-        nome: 'Jon Vlogs',
-        profissao: 'Youtuber',
-        envolvido: 'sim',
-        nivel: 'alto' //Nível de suspeita
-    }
+    
 ];
 
+//Rota para cadastrar um novo suspeito
+suspeitosRoutes.post("/", (req, res) => {
+    const { nome, profissao, envolvido, nivel } = req.body;
+  
+    // Validação dos campos nome e profissão
+    if (!nome || !profissao) {
+      return res.status(400).send({
+        message: "O nome ou o profissão não foi preenchido!",
+      });
+    }
+  
+    // Validação do nível de suspeita
+    if (nivel != 'baixo' && nivel != 'médio' && nivel != 'alto') {
+      return res.status(400).send({
+        message:
+          "O suspeito não possui o nível de suspeita!",
+      });
+    }
+  
+    // Criação de um novo suspeito
+    const novoSuspeito = {
+      nome,
+      profissao,
+      envolvido,
+      nivel,
+    };
+  
+    // Adiciona o novo suspeito ao array
+    suspeitos.push(novoSuspeito);
+  
+    return res.status(201).json({
+      message: "Suspeito cadastrado com sucesso!",
+      novoSuspeito,
+    });
+  });
+
+//Rota para buscar os suspeitos
 suspeitosRoutes.get("/", (req, res) => {
     return res.status(200).json(suspeitos);
 })
